ecfc55dd7424e3462f0a35eff60bf9bf5eff1615
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-11-13 10:26:24 -0300

    feat: first commit

---
+++ src/design/theme/index.js
@@ -0,0 +1,19 @@
+const Theme = {
+  colors: {
+    background: '#eeee',
+    primary: '#75F7FF',
+    black: '#000000',
+  },
+  spacing: {
+    n2: '2px',
+    n4: '4px',
+    n6: '6px',
+    n8: '8px',
+    n12: '12px',
+    n16: '16px',
+    n24: '24px',
+    n32: '32px',
+    n40: '40px',
+  },
+};
+export default Theme;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Home = props => {
+  <View>
+    <Text>HypeHere</Text>
+  </View>;
+};
+export default Home;
