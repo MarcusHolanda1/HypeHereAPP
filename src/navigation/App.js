3fd6741711ae4c6faf474837e32c6c66eec5802d
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-11-27 22:12:55 -0300

    feat: adicionando context no navigation

--- src/navigation/App.js
+++ src/navigation/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
-import Login from '../../src/screens/Login';
 import Home from '../screens/Home';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {SneakersContext} from '../contexts/SneakersContext';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{headerShown: false}}>
-        <Tab.Screen name="Home" component={Home} />
-      </Tab.Navigator>
+      <SneakersContext>
+        <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={{headerShown: false}}>
+          <Tab.Screen name="Home" component={Home} />
+        </Tab.Navigator>
+      </SneakersContext>
     </NavigationContainer>
   );
 }
