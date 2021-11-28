a8d2772581833eb07e675898d0d05ffdde9d6b21
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-11-27 22:15:50 -0300

    feat: consumindo api no contextSneakers

---
+++ src/contexts/SneakersContext/index.js
@@ -0,0 +1,30 @@
+import React, {useState, createContext, useEffect, useCallback} from 'react';
+import request from '../../services';
+import axios from 'axios';
+
+export const ContextSneakers = createContext();
+
+export const SneakersContext = ({children}) => {
+  const [sneakers, setSneakers] = useState([]);
+
+  const getAPI = useCallback(async () => {
+    request.get().then(({data}) => {
+      setSneakers(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    getAPI();
+  }, [getAPI]);
+  console.log(sneakers);
+
+  return (
+    <ContextSneakers.Provider
+      value={{
+        sneakers,
+        setSneakers,
+      }}>
+      {children}
+    </ContextSneakers.Provider>
+  );
+};
