e6fc9b1e60a688301a3a92b832992f63ca7721f8
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-02 22:39:39 -0300

    fix : corrigindo estrutura da página

--- src/design/structures/Page/styles.js
+++ src/design/structures/Page/styles.js
@@ -4,24 +4,6 @@ import Theme from '../../theme';
 export const Container = styled.View`
   flex: 1;
   background-color: ${Theme.colors.background};
-`;
-
-export const SafeContainer = styled.SafeAreaView`
-  flex: 1;
-  background-color: ${Theme.colors.background};
-`;
-
-export const Content = styled.View`
   padding-top: ${Theme.spacing.n16};
   padding-left: ${Theme.spacing.n16};
-`;
-
-export const ScrollContent = styled.ScrollView`
-  padding-top: ${Theme.spacing.n16};
-  padding-left: ${Theme.spacing.n16};
-`;
-
-export const KeyboardContainer = styled.KeyboardAvoidingView`
-  flex: 1;
-  background-color: ${Theme.colors.background};
 `;
