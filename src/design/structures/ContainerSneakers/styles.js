1e6b03e7741498cee30e32decebffb7c2dedc4dc
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-02 22:41:47 -0300

    feat : alterações de layout

--- src/design/structures/ContainerSneakers/styles.js
+++ src/design/structures/ContainerSneakers/styles.js
@@ -4,6 +4,7 @@ import Theme from '../../theme';
 export const Container = styled.SafeAreaView`
   background-color: ${Theme.colors.background};
   width: 300px;
+  flex: 1;
 `;
 
 export const SafeContainer = styled.ScrollView``;
--- src/design/structures/Page/index.js
+++ src/design/structures/Page/index.js
@@ -1,25 +1,9 @@
-import React, {useRef} from 'react';
-import {Platform} from 'react-native';
+import React from 'react';
 
 import * as S from './styles';
 
-const Page = ({children, scrolled}) => {
-  const scrollRef = useRef(null);
-  return (
-    <S.Container>
-      <S.SafeContainer>
-        {scrolled ? (
-          <S.KeyboardContainer
-            keyboardVerticalOffset={8}
-            behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
-            <S.ScrollContent ref={scrollRef}>{children}</S.ScrollContent>
-          </S.KeyboardContainer>
-        ) : (
-          <S.Content>{children}</S.Content>
-        )}
-      </S.SafeContainer>
-    </S.Container>
-  );
+const Page = ({children}) => {
+  return <S.Container>{children}</S.Container>;
 };
 
 export default Page;
