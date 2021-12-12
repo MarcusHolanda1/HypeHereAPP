e1082352b2fe4e1f11d410a8b28c7fffbe3f70c8
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-12 20:37:23 -0300

    ref: retirando width do Card Sneaker

--- src/design/components/CardSneakers/styles.js
+++ src/design/components/CardSneakers/styles.js
@@ -4,8 +4,6 @@ import Theme from '../../theme';
 export const Card = styled.View`
   background-color: ${Theme.colors.white};
   border-radius: 24px;
-  width: 297px;
-  height: 370px;
   position: relative;
   margin-bottom: ${Theme.spacing.n24};
   padding: ${Theme.spacing.n32};
