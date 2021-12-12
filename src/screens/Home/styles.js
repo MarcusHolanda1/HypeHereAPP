ae434823dad2ba067fe2faa7ebba6bd8856fd687
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-12 20:42:09 -0300

    style: removendo import primaryButton

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components/native';
 import Theme from '../../design/theme';
-import PrimaryButton from '../../design/components/Buttons/PrimaryButton';
 
 export const ContentHeader = styled.View`
   flex-direction: row;
