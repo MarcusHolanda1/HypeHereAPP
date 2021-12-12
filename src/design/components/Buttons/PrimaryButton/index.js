d5243b8a029976c2232198be3aff3eb23608e540
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-11 23:31:39 -0300

    feat: onPress no button

--- src/design/components/Buttons/PrimaryButton/index.js
+++ src/design/components/Buttons/PrimaryButton/index.js
@@ -2,8 +2,12 @@ import * as S from './styles';
 import {View, Text} from 'react-native';
 import React from 'react';
 
-const PrimaryButton = ({text, background, color, width, heigth}) => (
-  <S.WrapButton background={background} width={width} heigth={heigth}>
+const PrimaryButton = ({onPress, text, background, color, width, heigth}) => (
+  <S.WrapButton
+    onPress={onPress}
+    background={background}
+    width={width}
+    heigth={heigth}>
     <S.CartIcon />
     <S.TextButton color={color}>{text}</S.TextButton>
   </S.WrapButton>
