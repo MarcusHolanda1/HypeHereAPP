e85b46c245eebbc36ad7a807d51d5bc87c556caf
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-12 20:37:53 -0300

    feat: H1 Price criado

--- src/design/components/Text/index.js
+++ src/design/components/Text/index.js
@@ -1,13 +1,15 @@
 import React, {useCallback} from 'react';
 import {View} from 'react-native';
 
-import {H1, H2, P} from './styles';
+import {H1, H2, P, H1PRICE} from './styles';
 
 const Text = ({type, ...rest}) => {
   const render = useCallback(() => {
     switch (type) {
       case 'h1':
         return <H1>{rest.children}</H1>;
+      case 'h1Price':
+        return <H1PRICE>{rest.children}</H1PRICE>;
       case 'h2':
         return <H2>{rest.children}</H2>;
       case 'p':
