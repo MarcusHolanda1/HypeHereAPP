10d1de780287b1ec5ed093762854bc781204b8f7
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-12 20:43:39 -0300

    style: estilização viewSneaker

---
+++ src/screens/ViewSneaker/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components/native';
+import Theme from '../../design/theme';
+
+export const ContentCard = styled.View`
+  padding-right: ${Theme.spacing.n16};
+`;
+
+export const ContentHandles = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: ${Theme.spacing.n12};
+  margin-bottom: ${Theme.spacing.n40};
+`;
+
+export const ContentName = styled.View``;
+
+export const ContentPrice = styled.View`
+  align-self: flex-end;
+`;
+
+export const ContentImage = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContentStars = styled.View`
+  flex-direction: row;
+  margin-top: ${Theme.spacing.n40};
+`;
+
+export const ContentSize = styled.View`
+  margin-top: ${Theme.spacing.n24};
+`;
+
+export const ContentSizeTouch = styled.SafeAreaView``;
+
+export const ContentButtonBuy = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 14px;
+`;
