3aebb197900302fb6e70312ff7ced0a5fce95df9
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-26 13:15:15 -0300

    style: style span icon

---
+++ src/design/components/SpanIcons/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding-top: 9px;
+`;
+
+export const ContentSpan = styled.View`
+  top: -29px;
+  left: 21px;
+  width: 12px;
+`;
