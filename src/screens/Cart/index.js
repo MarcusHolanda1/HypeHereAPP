a50100ae3e00a157fecef06f9a9b541ea64e3332
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-26 17:39:19 -0300

    feat: iniciando com a screen cart

--- src/screens/Cart/index.js
+++ src/screens/Cart/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-
 import * as S from './styles';
+import {CardCart, FavoriteListStructure, Text} from '../../design';
+import Logo from '../../assets/icons/logo/HYPEHERE.svg';
 
-const Cart = () => {
-  return <></>;
+const Cart = ({route, navigation}) => {
+  console.log(route.params);
+  return (
+    <FavoriteListStructure>
+      <Logo width={150} height={35} />
+      <S.ContentTitle>
+        <Text type="h1">Meu carrinho</Text>
+      </S.ContentTitle>
+
+      <S.ContentCards>
+        <CardCart width="370px"></CardCart>
+      </S.ContentCards>
+    </FavoriteListStructure>
+  );
 };
 export default Cart;
---
+++ src/screens/Cart/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+import Theme from '../../design/theme';
+
+export const ContentCards = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContentTitle = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: ${Theme.spacing.n32};
+  margin-bottom: ${Theme.spacing.n32};
+`;
