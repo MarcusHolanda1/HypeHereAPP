7fa12ea039c399f30b2e71a6103e4160e18e6f69
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-26 13:14:12 -0300

    ref: refatorando para as imagens com span tab navigation

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,15 @@
 const IMAGES = {
   inactive: {
     cart: require('./icons/tabNavigation/cartInactive.png'),
-    favorite: require('./icons/tabNavigation/favoriteInactive.png'),
+    favorite: require('./icons/tabNavigation/favoriteInaactive.png'),
+    favoriteSpan: require('./icons/tabNavigation/favoriteInactiveSpan.png'),
     home: require('./icons/tabNavigation/homeInactive.png'),
     user: require('./icons/tabNavigation/userInactive.png'),
   },
   active: {
     cart: require('./icons/tabNavigation/cartActive.png'),
-    favorite: require('./icons/tabNavigation/favoriteActive.png'),
+    favorite: require('./icons/tabNavigation/favoriteAactive.png'),
+    favoriteSpan: require('./icons/tabNavigation/favoriteAactiveSpan.png'),
     home: require('./icons/tabNavigation/homeActive.png'),
     user: require('./icons/tabNavigation/userActive.png'),
   },
@@ -19,6 +21,13 @@ const IMAGES = {
   stars: {
     starFull: require('./icons/stars/starFull.png'),
     starHalf: require('./icons/stars/starHalf.png'),
+  },
+  logos: {
+    logo: require('./icons/logo/logo.png'),
+  },
+  teste: {
+    favoriteDone: require('./icons/test/favoriteDone.png'),
+    favoriteNotDone: require('./icons/test/favoriteNotDone.png'),
   },
 };
 
