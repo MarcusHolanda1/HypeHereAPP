a057605da4722fa2859e28663b45317eca1162df
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-25 22:45:30 -0300

    feat: adicionando width via props no card

--- src/design/components/CardSneakers/index.js
+++ src/design/components/CardSneakers/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as S from './styles';
 
-const CardSneakers = props => {
-  return <S.Card>{props.children}</S.Card>;
+const CardSneakers = ({children, width}) => {
+  return <S.Card width={width}>{children}</S.Card>;
 };
 export default CardSneakers;
