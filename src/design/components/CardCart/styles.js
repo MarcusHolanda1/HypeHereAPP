ee277df868c1d347b0de55c47510287627496c27
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-26 20:09:53 -0300

    ref: refatorando carCart

--- src/design/components/CardCart/styles.js
+++ src/design/components/CardCart/styles.js
@@ -4,6 +4,6 @@ import Theme from '../../theme';
 export const Card = styled.View`
   background-color: ${Theme.colors.white};
   border-radius: 24px;
-  padding: ${Theme.spacing.n12};
   width: ${props => props.width};
+  margin-bottom: ${Theme.spacing.n16};
 `;
