525623e6c89048d5ca94e6d7596683f5728f8a8f
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-26 17:33:55 -0300

    feat: adicionando cardCart e detailsBUtton

--- src/design/components/index.js
+++ src/design/components/index.js
@@ -1,8 +1,9 @@
 export {default as PrimaryButton} from './Buttons/PrimaryButton';
-export {default as BrandButton} from './Buttons/BrandButton';
 export {default as Text} from './Text/index';
 export {default as SearchBar} from './SearchBar';
 export {default as IconGlobal} from './IconGlobal';
 export {default as CardSneakers} from './CardSneakers';
 export {default as BackButton} from './Buttons/BackButton';
 export {default as SpanIcon} from './SpanIcons';
+export {default as CardCart} from './CardCart';
+export {default as DetailsButton} from './Buttons/DetailsButton';
