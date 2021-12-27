d81346406bb3f918b73134ea68dd89c1c4d721b5
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-27 20:23:50 -0300

    feat: cart add e remove context

--- src/contexts/CartContext/index.js
+++ src/contexts/CartContext/index.js
@@ -7,13 +7,53 @@ export const CartContext = ({children}) => {
 
   const addCartSneaker = useCallback(
     item => {
-      setCartSneakers([...cartSneakers, item]);
+      const exist = cartSneakers.find(each => each.id === item.id);
+      if (exist) {
+        setCartSneakers(
+          cartSneakers.map(each =>
+            each.id === item.id ? {...exist, qty: exist.qty + 1} : each,
+          ),
+        );
+      } else {
+        setCartSneakers([...cartSneakers, {...item, qty: 1}]);
+      }
+    },
+    [cartSneakers],
+  );
+
+  const removeSneakerCart = useCallback(
+    item => {
+      const exist = cartSneakers.find(each => each.id === item.id);
+      if (exist.qty === 1) {
+        setCartSneakers(cartSneakers.filter(each => each.id !== item.id));
+      } else {
+        setCartSneakers(
+          cartSneakers.map(each =>
+            each.id === item.id ? {...exist, qty: exist.qty - 1} : each,
+          ),
+        );
+      }
+    },
+    [cartSneakers],
+  );
+
+  const trashSneakerCart = useCallback(
+    item => {
+      const index = cartSneakers.findIndex(each => each.id === item.id);
+      cartSneakers.splice(index, 1);
+      setCartSneakers([...cartSneakers]);
     },
     [cartSneakers],
   );
 
   return (
-    <ContextCart.Provider value={{cartSneakers, addCartSneaker}}>
+    <ContextCart.Provider
+      value={{
+        cartSneakers,
+        addCartSneaker,
+        removeSneakerCart,
+        trashSneakerCart,
+      }}>
       {children}
     </ContextCart.Provider>
   );
