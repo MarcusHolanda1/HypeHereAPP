82d6a6fd3572bd7ae58b4c5619c7b33931dced18
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-27 20:25:59 -0300

    feat: screen cart parcialmente criada

--- src/screens/Cart/index.js
+++ src/screens/Cart/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Image} from 'react-native';
 import * as S from './styles';
-import {CardCart, FavoriteListStructure, Text} from '../../design';
+import {
+  CardCart,
+  FavoriteListStructure,
+  Text,
+  TrashButton,
+  AddAndRemoveButton,
+} from '../../design';
 import Logo from '../../assets/icons/logo/HYPEHERE.svg';
+import {ContextCart} from '../../contexts/CartContext';
+import IMAGES from '../../assets';
 
 const Cart = ({route, navigation}) => {
-  console.log(route.params);
+  const {cartSneakers, addCartSneaker, removeSneakerCart, trashSneakerCart} =
+    useContext(ContextCart);
+  console.log(cartSneakers);
+
   return (
     <FavoriteListStructure>
       <Logo width={150} height={35} />
@@ -12,9 +24,40 @@ const Cart = ({route, navigation}) => {
         <Text type="h1">Meu carrinho</Text>
       </S.ContentTitle>
 
-      <S.ContentCards>
-        <CardCart width="370px"></CardCart>
-      </S.ContentCards>
+      <S.ContainerCards>
+        {cartSneakers.map((cartSneaker, index) => {
+          return (
+            <CardCart width="370px" key={index}>
+              <S.ContentCard>
+                <S.ContentImage>
+                  <Image
+                    source={{uri: cartSneaker.image}}
+                    style={{width: 130, height: 100}}
+                    resizeMode="contain"
+                  />
+                </S.ContentImage>
+                <S.ContentShoe>
+                  <Text type="h3">{cartSneaker.shoe}</Text>
+                  <S.ContentAddAndRemoveButton>
+                    <AddAndRemoveButton
+                      onPress={() => removeSneakerCart(cartSneaker)}
+                      source={IMAGES.handle.remove}
+                    />
+                    <Text type="h3">{cartSneaker.qty}</Text>
+                    <AddAndRemoveButton
+                      onPress={() => addCartSneaker(cartSneaker)}
+                      source={IMAGES.handle.add}
+                    />
+                  </S.ContentAddAndRemoveButton>
+                </S.ContentShoe>
+                <S.ContentTrash>
+                  <TrashButton onPress={() => trashSneakerCart(cartSneaker)} />
+                </S.ContentTrash>
+              </S.ContentCard>
+            </CardCart>
+          );
+        })}
+      </S.ContainerCards>
     </FavoriteListStructure>
   );
 };
--- src/screens/Cart/styles.js
+++ src/screens/Cart/styles.js
@@ -1,15 +1,44 @@
 import styled from 'styled-components/native';
 import Theme from '../../design/theme';
 
-export const ContentCards = styled.View`
+export const ContainerCards = styled.View`
   justify-content: center;
   align-items: center;
 `;
 
 export const ContentTitle = styled.View`
-  display: flex;
   justify-content: center;
   align-items: center;
   margin-top: ${Theme.spacing.n32};
   margin-bottom: ${Theme.spacing.n32};
+`;
+
+export const ContentTrash = styled.View`
+  align-self: flex-end;
+`;
+
+export const ContentCard = styled.View`
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const ContentImage = styled.View`
+  align-self: flex-start;
+  justify-content: center;
+  align-items: center;
+  top: 30px;
+  margin-left: ${Theme.spacing.n16};
+`;
+
+export const ContentShoe = styled.View`
+  flex: 1;
+  margin-top: ${Theme.spacing.n16};
+`;
+
+export const ContentAddAndRemoveButton = styled.View`
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  padding: ${Theme.spacing.n16};
+  margin-top: ${Theme.spacing.n24};
 `;
