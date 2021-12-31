1621364c683e71fea0ced5da212ec3213defcdc4
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-12-31 12:41:35 -0300

    feat: omggg quase funcionando

--- src/screens/User/index.js
+++ src/screens/User/index.js
@@ -1,7 +1,42 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-community/google-signin';
+import Authentication from '../../testGoogle/Authentication';
+import Authenticated from '../../testGoogle/Authenticated';
 
-const User = ({navigation}) => {
-  return <View></View>;
-};
-export default User;
+export default function User() {
+  const [authenticated, setAuthenticated] = useState(false);
+  console.log('auth aqui', authenticated);
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        '125238915412-81rs3odhre0au7qsc0geoua8dc052h9i.apps.googleusercontent.com',
+    });
+  }, []);
+
+  async function onGoogleButtonPress() {
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
+
+  auth().onAuthStateChanged(user => {
+    if (user) {
+      setAuthenticated(true);
+    } else {
+      setAuthenticated(false);
+    }
+  });
+
+  if (authenticated) {
+    return <Authenticated />;
+  }
+
+  return <Authentication onGoogleButtonPress={onGoogleButtonPress} />;
+}
