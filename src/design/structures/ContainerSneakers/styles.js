cdc37b2ef2fd8778ff36b1954accf7f0a61c9afe
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-01-01 20:15:05 -0300

    style: retirando padding

--- src/design/structures/ContainerSneakers/styles.js
+++ src/design/structures/ContainerSneakers/styles.js
@@ -5,6 +5,7 @@ export const Container = styled.SafeAreaView`
   background-color: ${Theme.colors.background};
   width: 300px;
   flex: 1;
+  padding-right: ${Theme.spacing.n16};
 `;
 
 export const SafeContainer = styled.ScrollView``;
