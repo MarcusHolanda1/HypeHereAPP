eb855155224b8100341c90d82234a1e44903d6b9
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2022-01-02 01:21:37 -0300

    ref: diminuindo padding top

--- src/design/structures/Page/styles.js
+++ src/design/structures/Page/styles.js
@@ -4,6 +4,6 @@ import Theme from '../../theme';
 export const Container = styled.View`
   flex: 1;
   background-color: ${Theme.colors.background};
-  padding-top: ${Theme.spacing.n16};
+  padding-top: ${Theme.spacing.n8};
   padding-left: ${Theme.spacing.n16};
 `;
